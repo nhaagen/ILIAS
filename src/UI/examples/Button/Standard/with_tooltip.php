6ab59fd8a76cace2ec5d6ef905689482364597c6
Richard Klees <richard.klees@concepts-and-training.de>
2023-02-27 10:38:09 +0100

    WIP

--- src/UI/examples/Button/Standard/with_tooltip.php
+++ src/UI/examples/Button/Standard/with_tooltip.php
@@ -13,7 +13,7 @@ function with_tooltip()
     $button = $f->button()
         ->standard("Goto ILIAS", "http://www.ilias.de")
         ->withHelpTopics(
-            ...$f->helpTopics("ilias")
+            ...$f->helpTopics("ilias", "learning management system")
         );
 
     return $renderer->render($button);
