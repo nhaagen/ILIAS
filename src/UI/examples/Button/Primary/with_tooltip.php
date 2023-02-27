fb822df03193cdf97dc57fa2cd143639d4c53677
Nils Haagen <nhaagen@cat06.de>
2023-02-27 21:22:13 +0100

    UI: HelpTopics, fix copyrights

--- src/UI/examples/Button/Primary/with_tooltip.php
+++ src/UI/examples/Button/Primary/with_tooltip.php
@@ -17,28 +17,16 @@
 
 declare(strict_types=1);
 
-namespace ILIAS\UI\examples\Button\Tag;
+namespace ILIAS\UI\examples\Button\Primary;
 
 function with_tooltip()
 {
     global $DIC;
     $f = $DIC->ui()->factory();
     $renderer = $DIC->ui()->renderer();
-
-    $tag = $f->button()->tag("simple tag", "#")
-        ->withEngagedState(true)
+    $primary =  $f->button()->primary("Goto ILIAS", "http://www.ilias.de")
         ->withHelpTopics(
             ...$f->helpTopics("ilias", "learning management system")
         );
-
-    $possible_relevances = array(
-        $tag::REL_MID,
-        $tag::REL_HIGH
-    );
-
-    foreach ($possible_relevances as $w) {
-        $buffer[] = $renderer->render($tag->withRelevance($w));
-    }
-
-    return implode(' ', $buffer);
+    return $renderer->render($primary);
 }
