c2af07cfe382e6d3d5cd58bf99ef772ed54dae6b
Nils Haagen <nhaagen@cat06.de>
2025-01-09 08:46:46 +0100

    UI/Navigation: rebase

--- components/ILIAS/Dashboard/Achievements/classes/class.ilAchievementsGUI.php
+++ components/ILIAS/Dashboard/Achievements/classes/class.ilAchievementsGUI.php
@@ -19,7 +19,7 @@
 declare(strict_types=1);
 
 /**
- * @ilCtrl_Calls ilAchievementsGUI: ilPersonalSkillsGUI, ilBadgeProfileGUI, ilLearningHistoryGUI, ilLPPersonalGUI
+ * @ilCtrl_Calls ilAchievementsGUI: ilPersonalSkillsGUI, ilBadgeProfileGUI, ilLearningHistoryGUI, ilLPPersonalGUI, ilLearningProgressGUI
  */
 class ilAchievementsGUI
 {
