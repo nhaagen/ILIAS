09f10ad17612971c7892ffad98b247bf52400385
Ferdinand Engländer <ferdinand.englaender@concepts-and-training.de>
2025-10-29 11:53:00 +0100

    11/DataTable/ add button styling

--- components/ILIAS/UI/src/Implementation/Component/Table/Renderer.php
+++ components/ILIAS/UI/src/Implementation/Component/Table/Renderer.php
@@ -271,10 +271,11 @@ class Renderer extends AbstractComponentRenderer
         }
 
         if ($creation_prompt = $component->getEntryCreation()) {
+            $add_glyph = $this->getUIFactory()->symbol()->glyph()->add();
             $btn = $this->getUIFactory()->button()->standard(
                 $this->txt('datatable_add_entry'),
                 $creation_prompt->getShowSignal()
-            );
+            )->withSymbol($add_glyph);
             $tpl->setVariable('SPAWN_CONTROLS', $default_renderer->render($btn));
             $tpl->setVariable('SPAWN_CONTROLS_DIALOG', $default_renderer->render($creation_prompt));
         }
